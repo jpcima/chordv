3b76ca319d7b61030a234831b600496480a18c4a
Gilles Maire <gilles@gillesmaire.com>
2016-03-09 08:19:16 +0100

    Start of QLabel Example

---
+++ examplelabel.cpp
@@ -0,0 +1,7 @@
+#include "examplelabel.h"
+
+ExampleLabel::ExampleLabel()
+{
+
+}
+
---
+++ examplelabel.h
@@ -0,0 +1,15 @@
+#ifndef EXAMPLELABEL_H
+#define EXAMPLELABEL_H
+
+
+class ExampleLabel : public QLabel
+{
+public:
+    ExampleLabel();
+
+signals:
+
+public slots:
+};
+
+#endif // EXAMPLELABEL_H
