4e074e13c1d34d05a4eb8763b93b3a4a6b4362ae
Gilles Maire <gilles@lesjetesdelencre.com>
2017-05-09 10:34:26 +0200

    ChordDiagram run

--- chorddiagram.cpp
+++ chorddiagram.cpp
@@ -20,11 +20,11 @@ void ChordDiagram::paintEvent(QPaintEvent *e)
     int w=this->width();
     int cs=20;
     int hm=20;
-    int xmfret=-10;
-    int ymfret=+7;
+    int xmfret=-12;
+    int ymfret=+15;
     int x=w/2-3*cs;
     int y=hm;
-    int rcercle=10;
+    int rcercle=12;
     for ( int i=0;i<=5;i++)
     {
         painter.drawLine(x+cs*i,y,x+cs*i,h-hm);
@@ -33,10 +33,7 @@ void ChordDiagram::paintEvent(QPaintEvent *e)
     {
         painter.drawLine(x,y*i,x+cs*5,y*i);
     }
-    QPen pen;
-    pen.setWidth(3);
-    painter.setPen(pen);
-    painter.drawLine(x+1,y,x+cs*5+1,y);
+
     if ( !m_diagram.isEmpty())
     {
         QStringList list=m_diagram.split(" ");
@@ -44,9 +41,28 @@ void ChordDiagram::paintEvent(QPaintEvent *e)
         for ( int i=0;i<6;i++)
         {
             QString note=list.at(i+1);
-            if ( note!="x") painter.drawEllipse(x+cs*i,y*note.toInt(),rcercle,rcercle);
+            if (note == "0" )
+            {
+                QBrush brush(Qt::NoBrush);
+                painter.setBrush(brush);
+                painter.drawEllipse(x+cs*i-cs/4,y*note.toInt()+cs/4,rcercle,rcercle);
+            }
+            else if ( note!="x")
+            {
+                QBrush brush(Qt::SolidPattern);
+                painter.setBrush(brush);
+                painter.drawEllipse(x+cs*i-cs/4,y*note.toInt()+cs/2,rcercle,rcercle);
+            }
+            else
+            {
+                painter.drawText(QPoint(x+cs*i-cs/4,y*note.toInt()+cs/2),"x");
+            }
         }
 
     }
+    QPen pen;
+    pen.setWidth(3);
+    painter.setPen(pen);
+    painter.drawLine(x+1,y,x+cs*5+1,y);
 }
 
