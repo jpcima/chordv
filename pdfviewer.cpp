ff56cfc29bb027702752121479de5da18454d68e
Gilles Maire <gilles@gillesmaire.com>
2017-05-19 21:54:53 +0200

    l

---
+++ pdfviewer.cpp
@@ -0,0 +1,34 @@
+#include     "pdfviewer.h"
+
+#include <QSettings>
+#include <QProcess>
+#include <QMessageBox>
+#include <QDebug>
+
+PdfViewer::PdfViewer(QString filename,QWidget *parent): QWidget(parent)
+{
+    QSettings s;
+    if ( s.value("PDFReader").toString().isEmpty())
+    {
+        QMessageBox::warning(this,tr("Variable PDFREader not set in Configuration")
+                             ,tr("Go to Tools/Preference and set the PDF Reader name and path ")
+                             ,tr("Ok"));
+        m_process=0;
+
+    }
+    else
+    {
+        m_process = new QProcess(this);
+        connect ( m_process,SIGNAL(finished(int)),this,SLOT(endProcess(int)));
+        QStringList arg;
+        arg<<filename;
+        m_process->start(s.value("PDFReader").toString(),arg);
+    }
+}
+
+
+void PdfViewer::endProcess(int)
+{
+    if ( m_process != 0 )
+        delete m_process;
+}
