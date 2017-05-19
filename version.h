713aaaec77aca8bfb6f6371984a6886785433927
Gilles Maire <gilles@gillesmaire.com>
2017-05-19 21:54:53 +0200

    l

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 #define VERSION "v0.1"
-#define FULLVERSION "e90470b4e04ed5a80f76c2a746b10684ad71de18"
+#define FULLVERSION "e1ed4bd18a572a9ccb4f6b5a1c947dd2df0b0e4d"
 #define DATEUS "2017/05/19"
 #endif // VERSION_H
