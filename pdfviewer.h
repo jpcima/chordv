6543c91225e583af503afe7245e012c8588cade5
Gilles Maire <gilles@gillesmaire.com>
2017-05-20 05:55:33 +0200

    ok

--- pdfviewer.h
+++ pdfviewer.h
@@ -4,15 +4,17 @@
 #include <QProcess>
 #include <QWidget>
 
-class PdfViewer : public QWidget
+class PdfViewer
 {
     Q_OBJECT
 public:
-    PdfViewer( QString filename,QWidget *parent);
+    PdfViewer( QString filename);
 private slots:
     void endProcess(int);
 private:
     QProcess *m_process;
+signals:
+    void finished();
 };
 
 #endif // PDFVIEWER_H
