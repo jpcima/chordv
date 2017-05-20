040101ab71a3857dd808d8bcb2a90c38aa453a24
Gilles Maire <gilles@lesjetesdelencre.com>
2017-05-20 13:32:02 +0200

    ok

--- dialogabout.cpp
+++ dialogabout.cpp
@@ -1,11 +1,13 @@
 #include "dialogabout.h"
 #include "ui_dialogabout.h"
-
+#include "version.h"
 DialogAbout::DialogAbout(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::DialogAbout)
 {
     ui->setupUi(this);
+    ui->lineEditVersion->setText(QString(VERSION));
+    ui->textBrowserCredit->setOpenLinks(false);
 }
 
 DialogAbout::~DialogAbout()
