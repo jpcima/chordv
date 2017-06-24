b1e973ef56b9aa11a174d421db628e92c94ed6db
Gilles Maire <gilles@gillesmaire.com>
2017-06-24 22:02:21 +0200

    Majuscule in title vertical spacing works now

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 #define VERSION "v0.1"
-#define FULLVERSION "12aaba89505a1cf9b4ca972f4296aa1c560b7bb6"
+#define FULLVERSION "4583864196005af84729e12af69713fcbab47d62"
 #define DATEUS "2017/06/24"
 #endif // VERSION_H
